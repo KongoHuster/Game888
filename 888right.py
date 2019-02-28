6054cb1e04d01d5dcda6fb3ab4801e489a54c02f
yzh_Dian <2169218186@qq.com>
2019-02-28 23:23:31 +0800

    成功启动界面，添加下注数

--- 888right.py
+++ 888right.py
@@ -2,63 +2,67 @@ import os
 import re
 import time
 
-url = "curl 'https://ipm-tw.pragmaticplay.net/gs2c/v3/gameService' -H 'Referer: " \
-      "https://ipm-tw.pragmaticplay.net/gs2c/html5Game.do?jackpotid=0&gname=888%20Dragons&extGame=1&ext=0&cb_target" \
-      "=exist_tab&symbol=vs1dragon8&jurisdictionID=99&mgckey=AUTHTOKEN" \
-      "@e6be865a65d3f713a33c5d67d3def97b21e403ba5c0565dd1263381acabce051~stylename@biggaming~SESSION@d3b1a3c3-b21e" \
-      "-4f0e-8302-b3cd37ea5153&tabName=' -H 'Origin: https://ipm-tw.pragmaticplay.net' -H 'User-Agent: Mozilla/5.0 (" \
-      "Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36' " \
-      "-H 'Content-type: application/x-www-form-urlencoded' --data " \
-      "'action=doInit&symbol=vs1dragon8&cver=36308&index=1&counter=1&repeat=0&mgckey=AUTHTOKEN" \
-      "@e6be865a65d3f713a33c5d67d3def97b21e403ba5c0565dd1263381acabce051~stylename@biggaming~SESSION@d3b1a3c3-b21e" \
-      "-4f0e-8302-b3cd37ea5153' --compressed "
 
-indexNumber = int(url.split("&index=")[1].split("&counter=")[0]) + 1
-counter = int(url.split("&counter=")[1].split("&repeat=")[0]) + 2
+def gameStart(token, numberndex, numberCounter, ):
+    url = "curl 'https://ipm-tw.pragmaticplay.net/gs2c/v3/gameService' -H 'Referer: " \
+          "https://ipm-tw.pragmaticplay.net/gs2c/html5Game.do?jackpotid=0&gname=888%20Dragons&extGame=1&ext=0&cb_target" \
+          "=exist_tab&symbol=vs1dragon8&jurisdictionID=99&mgckey=AUTHTOKEN" \
+          "@e6be865a65d3f713a33c5d67d3def97b21e403ba5c0565dd1263381acabce051~stylename@biggaming~SESSION@d3b1a3c3-b21e" \
+          "-4f0e-8302-b3cd37ea5153&tabName=' -H 'Origin: https://ipm-tw.pragmaticplay.net' -H 'User-Agent: " \
+          "Mozilla/5.0 (" \
+          "Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 " \
+          "Safari/537.36' " \
+          "-H 'Content-type: application/x-www-form-urlencoded' --data " \
+          "'action=doInit&symbol=vs1dragon8&cver=36308&index=1&counter=1&repeat=0&mgckey=AUTHTOKEN" \
+          "@e6be865a65d3f713a33c5d67d3def97b21e403ba5c0565dd1263381acabce051~stylename@biggaming~SESSION@d3b1a3c3-b21e" \
+          "-4f0e-8302-b3cd37ea5153' --compressed "
 
-head = "curl" + url.split("curl")[1].split("--data")[0] + "--data "
+    indexNumber = int(url.split("&index=")[1].split("&counter=")[0]) + 1
+    counter = int(url.split("&counter=")[1].split("&repeat=")[0]) + 2
 
-pattern = re.compile(r'\d+')
+    head = "curl" + url.split("curl")[1].split("--data")[0] + "--data "
 
-while True:
-    data = '\'action=doSpin&symbol=vs1dragon8&c=1&l=1&index=' + str(indexNumber) + '&counter=' + str(
-        counter) + "&repeat=0" \
-           + "&mgckey" + url.split("&mgckey")[-1]
-    sendUrl = head + data
-    result = os.popen(sendUrl).readlines()
+    pattern = re.compile(r'\d+')
 
-    if result[0].startwith("nomoney="):
-        print("没钱了，请充值")
-        break
-
-    if result == ['unlogged']:
-        print("游戏发生错误")
-        print("请输入正确的url")
-        break
-
-    print(result)
-    if result[0].endswith("SystemError"):
-        print("游戏停止，请重新复制url")
-        break
-
-    number = pattern.findall(result[0])
-    if int(number[0]) == 0:
-        print("没中奖")
-        indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
-        counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
-        time.sleep(0.005)
-    else:
-        print("中奖了")
-        indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
-        counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
-        data = '\'symbol=vs1dragon8&action=doCollect&index=' + str(indexNumber) + '&counter=' + str(
+    while True:
+        data = '\'action=doSpin&symbol=vs1dragon8&c=1&l=1&index=' + str(indexNumber) + '&counter=' + str(
             counter) + "&repeat=0" \
-               + '&mgckey' + url.split("&mgckey")[-1]
+               + "&mgckey" + url.split("&mgckey")[-1]
         sendUrl = head + data
         result = os.popen(sendUrl).readlines()
+
+        if result[0].startwith("nomoney="):
+            print("没钱了，请充值")
+            break
+
+        if result == ['unlogged']:
+            print("游戏发生错误")
+            print("请输入正确的url")
+            break
+
         print(result)
-        indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
-        counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+        if result[0].endswith("SystemError"):
+            print("游戏停止，请重新复制url")
+            break
+
+        number = pattern.findall(result[0])
+        if int(number[0]) == 0:
+            print("没中奖")
+            indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+            counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+            time.sleep(0.005)
+        else:
+            print("中奖了")
+            indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+            counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+            data = '\'symbol=vs1dragon8&action=doCollect&index=' + str(indexNumber) + '&counter=' + str(
+                counter) + "&repeat=0" \
+                   + '&mgckey' + url.split("&mgckey")[-1]
+            sendUrl = head + data
+            result = os.popen(sendUrl).readlines()
+            print(result)
+            indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+            counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
 
 # print(head + data)
 # print("\n\n")
--- gameUi.py
+++ gameUi.py
@@ -7,8 +7,8 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -21,20 +21,26 @@ class Ui_Dialog(object):
         self.textBrowser.setGeometry(QtCore.QRect(10, 60, 261, 231))
         self.textBrowser.setObjectName("textBrowser")
         self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(290, 70, 91, 61))
+        self.pushButton.setGeometry(QtCore.QRect(290, 110, 91, 61))
         self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(300, 150, 60, 16))
+        self.label.setGeometry(QtCore.QRect(300, 190, 60, 16))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(350, 150, 60, 16))
+        self.label_2.setGeometry(QtCore.QRect(350, 190, 60, 16))
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(300, 180, 60, 16))
+        self.label_3.setGeometry(QtCore.QRect(300, 220, 60, 16))
         self.label_3.setObjectName("label_3")
         self.label_4 = QtWidgets.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(350, 180, 60, 16))
+        self.label_4.setGeometry(QtCore.QRect(350, 220, 60, 16))
         self.label_4.setObjectName("label_4")
+        self.textEdit_2 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_2.setGeometry(QtCore.QRect(330, 70, 51, 31))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(290, 80, 60, 16))
+        self.label_5.setObjectName("label_5")
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -47,14 +53,19 @@ class Ui_Dialog(object):
         self.label_2.setText(_translate("Dialog", "0"))
         self.label_3.setText(_translate("Dialog", "流水"))
         self.label_4.setText(_translate("Dialog", "0"))
+        self.textEdit_2.setHtml(_translate("Dialog",
+                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                           "p, li { white-space: pre-wrap; }\n"
+                                           "</style></head><body style=\" font-family:\'.SF NS Text\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">1</p></body></html>"))
+        self.label_5.setText(_translate("Dialog", "下注"))
 
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = Ui_Dialog()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)  # 创建一个QApplication，也就是你要开发的软件app
+    MainWindow = QtWidgets.QMainWindow()  # 创建一个QMainWindow，用来装载你需要的各种组件、控件
+    ui = Ui_Dialog()  # ui是你创建的ui类的实例化对象
+    ui.setupUi(MainWindow)  # 执行类中的setupUi方法，方法的参数是第二步中创建的QMainWindow
+    MainWindow.show()  # 执行QMainWindow的show()方法，显示这个QMainWindow
+    sys.exit(app.exec_())  # 使用exit()或者点击关闭按钮退出QApplication
