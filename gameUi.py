efc4440cf9558e0de4a413bdfca78cb3e35e4161
yzh_Dian <2169218186@qq.com>
2019-03-01 10:32:53 +0800

    解决多线程的问题

--- gameUi.py
+++ gameUi.py
@@ -6,11 +6,20 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-
+import os
+import re
+import time
+import threading
+import mythread
 
 class Ui_Dialog(object):
+    indexNumber = ""
+    counter = ""
+    start = True
+
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(400, 300)
@@ -49,28 +58,118 @@ class Ui_Dialog(object):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.pushButton.setText(_translate("Dialog", "开始"))
-        self.pushButton.clicked.connect(lambda:self.startPlay())
+        self.pushButton.clicked.connect(lambda: self.startPlay())
         self.label.setText(_translate("Dialog", "余额"))
         self.label_2.setText(_translate("Dialog", "0"))
         self.label_3.setText(_translate("Dialog", "流水"))
         self.label_4.setText(_translate("Dialog", "0"))
         self.textEdit_2.setHtml(_translate("Dialog",
-                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
+                                           "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n "
+                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style "
+                                           "type=\"text/css\">\n "
                                            "p, li { white-space: pre-wrap; }\n"
-                                           "</style></head><body style=\" font-family:\'.SF NS Text\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
-                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">1</p></body></html>"))
+                                           "</style></head><body style=\" font-family:\'.SF NS Text\'; "
+                                           "font-size:13pt; font-weight:400; font-style:normal;\">\n "
+                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
+                                           "margin-right:0px; -qt-block-indent:0; "
+                                           "text-indent:0px;\">1</p></body></html>"))
         self.label_5.setText(_translate("Dialog", "下注"))
 
     def startPlay(self):
+        if self.start is True:
+            self.pushButton.setText("关闭")
+            if self.textEdit.toPlainText() is "":
+                self.textBrowser.insertPlainText("第一行为空，请添加curl\n")
+                cursor = self.textBrowser.textCursor()
+                self.textBrowser.moveCursor(cursor.End)
+                return
 
-        if self.textEdit.toPlainText() is "":
-            self.textBrowser.insertPlainText("第一行为空，请添加curl\n")
+            if self.textEdit_2.toPlainText() is "":
+                self.textBrowser.insertPlainText("下注金额为空，请输入金额")
+                cursor = self.textBrowser.textCursor()
+                self.textBrowser.moveCursor(cursor.End)
+                return
 
-        if self.textEdit_2.toPlainText() is "":
-            self.textBrowser.insertPlainText("下注金额为空，请输入金额")
+            self.thread = mythread.MyThread()
+            self.thread.start()
+            # try:
+            # t = threading.Thread(target=self.printHH(),  args=(20,), daemon=True)
+            # t.start()
+            # t.join()
+        else:
+            self.pushButton.setText("开始")
+            self.start = False
+
+        self.textBrowser.insertPlainText("下注金额为空，请输入金额")
         cursor = self.textBrowser.textCursor()
         self.textBrowser.moveCursor(cursor.End)
+
+
+    def gameStart(self, url):
+        indexNumber = int(url.split("&index=")[1].split("&counter=")[0]) + 1
+        counter = int(url.split("&counter=")[1].split("&repeat=")[0]) + 2
+
+        head = "curl" + url.split("curl")[1].split("--data")[0] + "--data "
+
+        pattern = re.compile(r'\d+')
+
+        while True:
+            data = '\'action=doSpin&symbol=vs1dragon8&c=1&l=1&index=' + str(indexNumber) + '&counter=' + str(
+                counter) + "&repeat=0" \
+                   + "&mgckey" + url.split("&mgckey")[-1]
+            sendUrl = head + data
+            result = os.popen(sendUrl).readlines()
+
+            if result[0].startwith("nomoney="):
+                print("没钱了，请充值")
+                self.textBrowser.insertPlainText("没钱了，请充值")
+                cursor = self.textBrowser.textCursor()
+                self.textBrowser.moveCursor(cursor.End)
+                break
+
+            if result == ['unlogged']:
+                print("游戏发生错误")
+                print("请重新输入正确的url")
+                self.textBrowser.insertPlainText("游戏发生错误")
+                self.textBrowser.insertPlainText("请重新输入正确的url")
+                cursor = self.textBrowser.textCursor()
+                self.textBrowser.moveCursor(cursor.End)
+                break
+
+            print(result)
+            if result[0].endswith("SystemError"):
+                print("游戏停止，请重新复制url")
+                self.textBrowser.insertPlainText("游戏停止，请重新复制url")
+                cursor = self.textBrowser.textCursor()
+                self.textBrowser.moveCursor(cursor.End)
+                break
+
+            number = pattern.findall(result[0])
+            if int(number[0]) == 0:
+                print("没中奖")
+                self.textBrowser.insertPlainText("没中奖")
+                cursor = self.textBrowser.textCursor()
+                self.textBrowser.moveCursor(cursor.End)
+                indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+                time.sleep(0.005)
+            else:
+                print("中奖了")
+                indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+                data = '\'symbol=vs1dragon8&action=doCollect&index=' + str(indexNumber) + '&counter=' + str(
+                    counter) + "&repeat=0" \
+                       + '&mgckey' + url.split("&mgckey")[-1]
+                sendUrl = head + data
+                result = os.popen(sendUrl).readlines()
+                print(result)
+                self.textBrowser.insertPlainText("中奖了")
+                self.textBrowser.insertPlainText(result)
+                cursor = self.textBrowser.textCursor()
+                self.textBrowser.moveCursor(cursor.End)
+                indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
 
 
 if __name__ == "__main__":
---
+++ mythread.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSignal
+
+
+class MyThread(QtCore.QThread):
+    # python3,pyqt5与之前的版本有些不一样
+    #  通过类成员对象定义信号对象
+    # _signal = pyqtSignal(str)
+
+    trigger = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(MyThread, self).__init__()
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        # 处理你要做的业务逻辑，这里是通过一个回调来处理数据，这里的逻辑处理写自己的方法
+        # wechat.start_auto(self.callback)
+        # self._signal.emit(msg);  可以在这里写信号焕发
+        for i in range(203300030):
+            print("12")
+            pass
+        self.trigger.emit()
+        # self._signal.emit(msg)
+
+    def callback(self, msg):
+        # 信号焕发，我是通过我封装类的回调来发起的
+        # self._signal.emit(msg)
+        pass
