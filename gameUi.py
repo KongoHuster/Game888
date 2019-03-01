17fae8e569d6b8459f170f6d87f9b44a1d6b60c2
yzh_Dian <2169218186@qq.com>
2019-03-01 10:59:56 +0800

    完成线程调整

--- gameUi.py
+++ gameUi.py
@@ -9,15 +9,17 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-import os
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSignal
 import re
+import os
 import time
-import threading
-import mythread
+
+indexNumber = ""
+counter = ""
 
 class Ui_Dialog(object):
-    indexNumber = ""
-    counter = ""
+
     start = True
 
     def setupUi(self, Dialog):
@@ -77,12 +79,12 @@ class Ui_Dialog(object):
                 return
 
             if self.textEdit_2.toPlainText() is "":
-                self.textBrowser.insertPlainText("下注金额为空，请输入金额")
+                self.textBrowser.insertPlainText("下注金额为空，请输入金额\n")
                 cursor = self.textBrowser.textCursor()
                 self.textBrowser.moveCursor(cursor.End)
                 return
 
-            self.thread = mythread.MyThread()
+            self.thread = MyThread(self.textEdit.toPlainText())
             self.thread.start()
             # try:
             # t = threading.Thread(target=self.printHH(),  args=(20,), daemon=True)
@@ -93,76 +95,109 @@ class Ui_Dialog(object):
             self.start = True
             self.thread.setstartBool()
 
-        self.textBrowser.insertPlainText("下注金额为空，请输入金额")
+        self.textBrowser.insertPlainText("下注金额为空，请输入金额\n")
         cursor = self.textBrowser.textCursor()
         self.textBrowser.moveCursor(cursor.End)
 
+class MyThread(QtCore.QThread):
+    # python3,pyqt5与之前的版本有些不一样
+    #  通过类成员对象定义信号对象
+    # _signal = pyqtSignal(str)
+    startBool = True
+    trigger = pyqtSignal()
+
+    def __init__(self, urlstr):
+        super(MyThread, self).__init__()
+        self.url = urlstr
+
+    def __del__(self):
+        self.wait()
+
+    def setstartBool(self):
+        self.startBool = False
+
+    def run(self):
+        # 处理你要做的业务逻辑，这里是通过一个回调来处理数据，这里的逻辑处理写自己的方法
+        # wechat.start_auto(self.callback)
+        # self._signal.emit(msg);  可以在这里写信号焕发
+        while self.startBool:
+            self.gameStart(self.url)
+            pass
+        self.trigger.emit()
+        # self._signal.emit(msg)
+
+    def callback(self, msg):
+        # 信号焕发，我是通过我封装类的回调来发起的
+        # self._signal.emit(msg)
+        pass
 
     def gameStart(self, url):
-        indexNumber = int(url.split("&index=")[1].split("&counter=")[0]) + 1
-        counter = int(url.split("&counter=")[1].split("&repeat=")[0]) + 2
+        try:
+            indexNumber = int(url.split("&index=")[1].split("&counter=")[0]) + 1
+            counter = int(url.split("&counter=")[1].split("&repeat=")[0]) + 2
 
-        head = "curl" + url.split("curl")[1].split("--data")[0] + "--data "
+            head = "curl" + url.split("curl")[1].split("--data")[0] + "--data "
 
-        pattern = re.compile(r'\d+')
+            pattern = re.compile(r'\d+')
 
-        while True:
-            data = '\'action=doSpin&symbol=vs1dragon8&c=1&l=1&index=' + str(indexNumber) + '&counter=' + str(
-                counter) + "&repeat=0" \
-                   + "&mgckey" + url.split("&mgckey")[-1]
-            sendUrl = head + data
-            result = os.popen(sendUrl).readlines()
-
-            if result[0].startwith("nomoney="):
-                print("没钱了，请充值")
-                self.textBrowser.insertPlainText("没钱了，请充值")
-                cursor = self.textBrowser.textCursor()
-                self.textBrowser.moveCursor(cursor.End)
-                break
-
-            if result == ['unlogged']:
-                print("游戏发生错误")
-                print("请重新输入正确的url")
-                self.textBrowser.insertPlainText("游戏发生错误")
-                self.textBrowser.insertPlainText("请重新输入正确的url")
-                cursor = self.textBrowser.textCursor()
-                self.textBrowser.moveCursor(cursor.End)
-                break
-
-            print(result)
-            if result[0].endswith("SystemError"):
-                print("游戏停止，请重新复制url")
-                self.textBrowser.insertPlainText("游戏停止，请重新复制url")
-                cursor = self.textBrowser.textCursor()
-                self.textBrowser.moveCursor(cursor.End)
-                break
-
-            number = pattern.findall(result[0])
-            if int(number[0]) == 0:
-                print("没中奖")
-                self.textBrowser.insertPlainText("没中奖")
-                cursor = self.textBrowser.textCursor()
-                self.textBrowser.moveCursor(cursor.End)
-                indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
-                counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
-                time.sleep(0.005)
-            else:
-                print("中奖了")
-                indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
-                counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
-                data = '\'symbol=vs1dragon8&action=doCollect&index=' + str(indexNumber) + '&counter=' + str(
+            while True:
+                data = '\'action=doSpin&symbol=vs1dragon8&c=1&l=1&index=' + str(indexNumber) + '&counter=' + str(
                     counter) + "&repeat=0" \
-                       + '&mgckey' + url.split("&mgckey")[-1]
+                       + "&mgckey" + url.split("&mgckey")[-1]
                 sendUrl = head + data
                 result = os.popen(sendUrl).readlines()
-                print(result)
-                self.textBrowser.insertPlainText("中奖了")
-                self.textBrowser.insertPlainText(result)
-                cursor = self.textBrowser.textCursor()
-                self.textBrowser.moveCursor(cursor.End)
-                indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
-                counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
 
+                if result[0].startwith("nomoney="):
+                    print("没钱了，请充值")
+                    self.textBrowser.insertPlainText("没钱了，请充值")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    break
+
+                if result == ['unlogged']:
+                    print("游戏发生错误")
+                    print("请重新输入正确的url")
+                    self.textBrowser.insertPlainText("游戏发生错误")
+                    self.textBrowser.insertPlainText("请重新输入正确的url")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    break
+
+                print(result)
+                if result[0].endswith("SystemError"):
+                    print("游戏停止，请重新复制url")
+                    self.textBrowser.insertPlainText("游戏停止，请重新复制url")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    break
+
+                number = pattern.findall(result[0])
+                if int(number[0]) == 0:
+                    print("没中奖")
+                    self.textBrowser.insertPlainText("没中奖")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                    counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+                    time.sleep(0.005)
+                else:
+                    print("中奖了")
+                    indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                    counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+                    data = '\'symbol=vs1dragon8&action=doCollect&index=' + str(indexNumber) + '&counter=' + str(
+                        counter) + "&repeat=0" \
+                           + '&mgckey' + url.split("&mgckey")[-1]
+                    sendUrl = head + data
+                    result = os.popen(sendUrl).readlines()
+                    print(result)
+                    self.textBrowser.insertPlainText("中奖了")
+                    self.textBrowser.insertPlainText(result)
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                    counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+        except:
+                print("url错误，请重新粘贴")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)  # 创建一个QApplication，也就是你要开发的软件app
--- mythread.py
+++ mythread.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSignal
-
+import re
+import os
+import time
 
 class MyThread(QtCore.QThread):
     # python3,pyqt5与之前的版本有些不一样
@@ -9,8 +11,9 @@ class MyThread(QtCore.QThread):
     startBool = True
     trigger = pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, urlstr):
         super(MyThread, self).__init__()
+        self.url = urlstr
 
     def __del__(self):
         self.wait()
@@ -23,7 +26,7 @@ class MyThread(QtCore.QThread):
         # wechat.start_auto(self.callback)
         # self._signal.emit(msg);  可以在这里写信号焕发
         while self.startBool:
-            print("12")
+            self.gameStart(self.url)
             pass
         self.trigger.emit()
         # self._signal.emit(msg)
@@ -32,3 +35,71 @@ class MyThread(QtCore.QThread):
         # 信号焕发，我是通过我封装类的回调来发起的
         # self._signal.emit(msg)
         pass
+
+    def gameStart(self, url):
+        try:
+            indexNumber = int(url.split("&index=")[1].split("&counter=")[0]) + 1
+            counter = int(url.split("&counter=")[1].split("&repeat=")[0]) + 2
+
+            head = "curl" + url.split("curl")[1].split("--data")[0] + "--data "
+
+            pattern = re.compile(r'\d+')
+
+            while True:
+                data = '\'action=doSpin&symbol=vs1dragon8&c=1&l=1&index=' + str(indexNumber) + '&counter=' + str(
+                    counter) + "&repeat=0" \
+                       + "&mgckey" + url.split("&mgckey")[-1]
+                sendUrl = head + data
+                result = os.popen(sendUrl).readlines()
+
+                if result[0].startwith("nomoney="):
+                    print("没钱了，请充值")
+                    self.textBrowser.insertPlainText("没钱了，请充值")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    break
+
+                if result == ['unlogged']:
+                    print("游戏发生错误")
+                    print("请重新输入正确的url")
+                    self.textBrowser.insertPlainText("游戏发生错误")
+                    self.textBrowser.insertPlainText("请重新输入正确的url")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    break
+
+                print(result)
+                if result[0].endswith("SystemError"):
+                    print("游戏停止，请重新复制url")
+                    self.textBrowser.insertPlainText("游戏停止，请重新复制url")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    break
+
+                number = pattern.findall(result[0])
+                if int(number[0]) == 0:
+                    print("没中奖")
+                    self.textBrowser.insertPlainText("没中奖")
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                    counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+                    time.sleep(0.005)
+                else:
+                    print("中奖了")
+                    indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                    counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+                    data = '\'symbol=vs1dragon8&action=doCollect&index=' + str(indexNumber) + '&counter=' + str(
+                        counter) + "&repeat=0" \
+                           + '&mgckey' + url.split("&mgckey")[-1]
+                    sendUrl = head + data
+                    result = os.popen(sendUrl).readlines()
+                    print(result)
+                    self.textBrowser.insertPlainText("中奖了")
+                    self.textBrowser.insertPlainText(result)
+                    cursor = self.textBrowser.textCursor()
+                    self.textBrowser.moveCursor(cursor.End)
+                    indexNumber = int(result[0].split("&index=")[1].split("&balance_cash=")[0]) + 1
+                    counter = int(result[0].split("&counter=")[1].split("&l=")[0]) + 2
+        except:
+                print("url错误，请重新粘贴")
