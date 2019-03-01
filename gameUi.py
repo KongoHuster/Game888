2a84eaefd37d8a53a0aa5f7877d57c5399564d9c
yzh_Dian <2169218186@qq.com>
2019-03-01 10:41:51 +0800

    调整按钮位置

--- gameUi.py
+++ gameUi.py
@@ -45,10 +45,10 @@ class Ui_Dialog(object):
         self.label_4.setGeometry(QtCore.QRect(350, 220, 60, 16))
         self.label_4.setObjectName("label_4")
         self.textEdit_2 = QtWidgets.QTextEdit(Dialog)
-        self.textEdit_2.setGeometry(QtCore.QRect(330, 70, 51, 31))
+        self.textEdit_2.setGeometry(QtCore.QRect(330, 70, 51, 25))
         self.textEdit_2.setObjectName("textEdit_2")
         self.label_5 = QtWidgets.QLabel(Dialog)
-        self.label_5.setGeometry(QtCore.QRect(290, 80, 60, 16))
+        self.label_5.setGeometry(QtCore.QRect(290, 75, 60, 16))
         self.label_5.setObjectName("label_5")
 
         self.retranslateUi(Dialog)
@@ -63,17 +63,7 @@ class Ui_Dialog(object):
         self.label_2.setText(_translate("Dialog", "0"))
         self.label_3.setText(_translate("Dialog", "流水"))
         self.label_4.setText(_translate("Dialog", "0"))
-        self.textEdit_2.setHtml(_translate("Dialog",
-                                           "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
-                                           "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n "
-                                           "<html><head><meta name=\"qrichtext\" content=\"1\" /><style "
-                                           "type=\"text/css\">\n "
-                                           "p, li { white-space: pre-wrap; }\n"
-                                           "</style></head><body style=\" font-family:\'.SF NS Text\'; "
-                                           "font-size:13pt; font-weight:400; font-style:normal;\">\n "
-                                           "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
-                                           "margin-right:0px; -qt-block-indent:0; "
-                                           "text-indent:0px;\">1</p></body></html>"))
+        self.textEdit_2.setText(_translate("Dialog", "10"))
         self.label_5.setText(_translate("Dialog", "下注"))
 
     def startPlay(self):
